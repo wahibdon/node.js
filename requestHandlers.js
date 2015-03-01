712d3a926d62973eb0698d837cd7c60367562840
Austin - Laptop <whibdon@ottcom.com>
2015-03-01 11:28:02 -0500

    lkj;lkj

--- requestHandlers.js
+++ requestHandlers.js
@@ -21,7 +21,7 @@ function start(response, postData){
 function upload(response, postData){
 	console.log("Request handler 'upload' was called");
 	response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Request for 'upload' received.");
+	response.write("You've sent: " + postData);
     response.end();
 }
 exports.start = start;
