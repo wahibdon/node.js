89d07ffa2c536ebca4dfda745b6f0bcac32415d0
Austin - Laptop <whibdon@ottcom.com>
2015-03-01 01:49:32 -0500

    it is apparently text/plain, not plain/text

--- router.js
+++ router.js
@@ -4,7 +4,7 @@ function route(handle, pathname, response){
 		handle[pathname](response);
 	}else{
 		console.log("No request handler found for "+pathname);
-		response.writeHead(200, {"Content-Type" : "plain/text"});
+		response.writeHead(404, {"Content-Type" : "text/plain"});
 		response.write("404 Not Found");
 		response.end();
 	}
