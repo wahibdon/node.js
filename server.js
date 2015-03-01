8e0e68e5ada0b8695b023426d980a86f2fb6e9ec
Austin - Laptop <whibdon@ottcom.com>
2015-03-01 11:25:09 -0500

    always a typo to fix

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var url = require("url");
 
 function start(route, handle) {
   function onRequest(request, response) {
+  	var postData = "";
     var pathname = url.parse(request.url).pathname;
     console.log("Request for " + pathname + " received.");
     request.setEncoding("utf8");
