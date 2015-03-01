85c3824a434777a25becd30344a40daa3f775686
Austin - Laptop <whibdon@ottcom.com>
2015-03-01 00:50:00 -0500

    server as module

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 var http = require("http");
-var i = 0;
-function onRequest(request, response) {
-  console.log("Request received."+i);
-  i++;
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
+
+function start() {
+  function onRequest(request, response) {
+    console.log("Request received.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(80);
+  console.log("Server has started.");
 }
 
-http.createServer(onRequest).listen(80);
-
-console.log("Server has started.");
+exports.start = start;
